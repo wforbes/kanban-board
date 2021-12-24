da58091b477b007f72ceb7d4e8f4954e7671ee7a
Will Forbes <wforbes87@gmail.com>
2021-12-24 14:04:34 -0800

    add "Fixing up the Backlog view" step

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -6,6 +6,7 @@
 				<div class="card-block">
 					<h5 class="card-title">
 						<span class="text-muted">#{{ item.id }}</span> {{ item.text }}
+						<span :class="badgeClass(item)">{{ badgeText(item) }}</span>
 					</h5>
 				</div>
 			</div>
@@ -22,11 +23,45 @@ export default {
 	components: {
 		"new-item": NewItemForm
 	},
+	data() {
+		return {
+			badgeDetail: {
+				todo: {
+					text: "to-do",
+					class: "badge badge-light"
+				},
+				inProgress: {
+					text: "in progress",
+					class: "badge badge-info"
+				},
+				done: {
+					text: "done",
+					class: "badge badge-success"
+				}
+			}
+		};
+	},
 	computed: mapState({
 		// note: using a 'getters' property in vuex file
 		//	and calling that with this.$store.getters.items
 		//	would be preferred
-		items: (s) => s.items.todo
-	})
+		items: (s) => [...s.items.todo, ...s.items.inProgress, ...s.items.done]
+	}),
+	methods: {
+		itemLane(item) {
+			if (this.$store.state.items.todo.includes(item)) {
+				return "todo";
+			} else if (this.$store.state.items.inProgress.includes(item)) {
+				return "inProgress";
+			}
+			return "done";
+		},
+		badgeText(item) {
+			return this.badgeDetail[this.itemLane(item)].text;
+		},
+		badgeClass(item) {
+			return this.badgeDetail[this.itemLane(item)].class;
+		}
+	}
 };
 </script>
