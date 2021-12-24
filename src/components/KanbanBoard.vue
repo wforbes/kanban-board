afc9b51662d76eb1f977d6a750e03e7570bc104b
Will Forbes <wforbes87@gmail.com>
2021-12-24 09:36:20 -0800

    add "Creating the Task Lanes.." and "Creating the Other Lanes" steps

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,9 +1,39 @@
 <template>
-	<p>This is where the kanban board will go</p>
+	<div class="board">
+		<div class="row">
+			<div class="col-md">
+				<task-lane id="todo" title="To-Do" :items="todoItems"></task-lane>
+			</div>
+			<div class="col-md">
+				<task-lane
+					id="inProgress"
+					title="In progress"
+					:items="inProgressItems"
+				></task-lane>
+			</div>
+			<div class="col-md">
+				<task-lane id="done" title="Done" :items="doneItems"></task-lane>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import TaskLane from "./TaskLane";
+
 export default {
-	name: "KanbanBoard"
+	name: "KanbanBoard",
+	components: {
+		"task-lane": TaskLane
+	},
+	// note: using a 'getters' property in vuex file
+	//	and calling that with this.$store.getters.items
+	//	would be preferred
+	computed: mapState({
+		todoItems: (s) => s.items.todo,
+		inProgressItems: (s) => s.items.inProgress,
+		doneItems: (s) => s.items.done
+	})
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,10 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		//note: Not sure of the purpose of creating an encapsulating
-		//	object for items, then a singular tense 'todo' array
-		//	actual structure should just be "todos: []", or "items: []"
 		items: {
-			todo: []
+			todo: [],
+			inProgress: [],
+			done: []
 		},
 		nextId: 1
 	},
