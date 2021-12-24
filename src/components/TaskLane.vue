4e66ea87c67df795e4883bf2ff1b2fe92c777fd4
Will Forbes <wforbes87@gmail.com>
2021-12-24 13:54:27 -0800

    add "Lanes & Drag/Drop Support" step

--- src/components/TaskLane.vue
+++ src/components/TaskLane.vue
@@ -2,9 +2,11 @@
 	<div class="card">
 		<h3 class="card-header">{{ title }}</h3>
 		<div class="card-body">
-			<div v-for="item in items" :key="item.id">
-				<item :item="item"></item>
-			</div>
+			<draggable v-model="draggables" :options="{ group: 'default ' }">
+				<div v-for="item in items" :key="item.id">
+					<item :item="item"></item>
+				</div>
+			</draggable>
 		</div>
 		<div class="card-footer text-muted">
 			{{ itemCount }}
@@ -12,19 +14,32 @@
 	</div>
 </template>
 <script>
+import Draggable from "vuedraggable";
 import TaskLaneItem from "./TaskLaneItem";
 
 export default {
 	name: "TaskLane",
 	props: ["items", "title", "id"],
 	components: {
-		item: TaskLaneItem
+		item: TaskLaneItem,
+		draggable: Draggable
 	},
 	computed: {
 		itemCount() {
 			if (!this.items) return "";
 			if (this.items.length === 1) return "1 task";
 			return `${this.items.length} tasks`;
+		},
+		draggables: {
+			get() {
+				return this.items;
+			},
+			set(items) {
+				this.$store.commit("updateItems", {
+					items,
+					id: this.id
+				});
+			}
 		}
 	}
 };
